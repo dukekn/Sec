5ecdab8e82fa59b04f6235021fd187bd4e6f2877
Georgi Pl. Berkovski <38726327+dukekn@users.noreply.github.com>
2020-12-19 11:25:43 +0200

    init

--- index.php
+++ index.php
@@ -27,14 +27,14 @@ $pricelist = [
 $terminal = new Lane\Core\Classes\Terminal($pricelist);
 
 //scenario 1 = 32.40
-//$terminal->scanItem("ZA");
-//$terminal->scanItem("YB");
-//$terminal->scanItem("FC");
-//$terminal->scanItem("GD");
-//$terminal->scanItem("ZA");
-//$terminal->scanItem("YB");
-//$terminal->scanItem("ZA");
-//$terminal->scanItem("ZA");
+$terminal->scanItem("ZA");
+$terminal->scanItem("YB");
+$terminal->scanItem("FC");
+$terminal->scanItem("GD");
+$terminal->scanItem("ZA");
+$terminal->scanItem("YB");
+$terminal->scanItem("ZA");
+$terminal->scanItem("ZA");
 
 //scenario 2 = 7.25
 //$terminal->scanItem("FC");
@@ -46,9 +46,9 @@ $terminal = new Lane\Core\Classes\Terminal($pricelist);
 //$terminal->scanItem("FC");
 
 //scenario 3 = 15.4
-$terminal->scanItem("ZA");
-$terminal->scanItem("YB");
-$terminal->scanItem("FC");
-$terminal->scanItem("GD");
+//$terminal->scanItem("ZA");
+//$terminal->scanItem("YB");
+//$terminal->scanItem("FC");
+//$terminal->scanItem("GD");
 
 $terminal->getTotal();
